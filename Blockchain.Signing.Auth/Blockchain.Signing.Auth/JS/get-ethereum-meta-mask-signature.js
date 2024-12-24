7c3fd0609f526c130f8c31d22c7d78a7fc699e97
msharp19 <matt19sharp@gmail.com>
2024-12-24 18:15:39 +0800

    Added test

---
+++ Blockchain.Signing.Auth/Blockchain.Signing.Auth/JS/get-ethereum-meta-mask-signature.js
@@ -0,0 +1,42 @@
+﻿const signMessage = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+            const account = accounts[0];
+            const raw_message = new Date().toISOString();
+            const network = "Ethereum";
+
+            const signature = await window.ethereum.request({
+                method: 'personal_sign',
+                params: [message, account],
+            });
+
+            console.log("Signature: " + signature);
+            console.log("Account: " + account);
+            console.log("Message: " + raw_message);
+
+            // Send to back-end
+            /*
+            const response = await fetch('/api/verify', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ raw_message, signature, network }),
+            });
+
+            const result = await response.json();
+            console.log(result);
+            */
+
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        console.log('MetaMask not detected');
+    }
+}
+
+await signMessage();
---
+++ Blockchain.Signing.Auth/Blockchain.Signing.Auth/JS/get-solana-phantom-signature.js
@@ -0,0 +1,55 @@
+﻿const getProvider = () => {
+    if ('phantom' in window) {
+        const provider = window.phantom?.solana;
+        if (provider?.isPhantom) {
+            return provider;
+        }
+    }
+
+    window.open('https://phantom.app/', '_blank');
+};
+
+const toHexString = (byteArray) => {
+    return Array.from(byteArray, function (byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+}
+
+const signMessage = async () => {
+    const provider = getProvider();
+    if (!provider) {
+        console.log('Phantom not detected');
+        return;
+    }
+
+    try {
+        const resp = await provider.connect();
+
+        const message = new Date().toISOString();
+        const encodedMessage = new TextEncoder().encode(message);
+        const signedMessage = await provider.signMessage(encodedMessage, "utf8");
+        const network = "Solana";
+
+        console.log("Public key: " + resp.publicKey.toString());
+        console.log("Signature (hex): " + toHexString(signedMessage.signature));
+        console.log("Message: " + message);
+
+        // Send to back-end
+        /*
+        const response = await fetch('/api/verify', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ raw_message, signature, network }),
+        });
+    
+        const result = await response.json();
+        console.log(result);
+        */
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+await signMessage();
